55ac1f8466cb6195fbf81c5daf678904c35f5ff5
dragon-fei <china_dragonfei@163.com>
2018-11-01 11:22:39 +0800

    first todo commit

---
+++ src/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      <input v-model="inputvalue"/>
+      <button @click="handelTodoList">提交</button>
+    </div>
+    <ul>
+      <todo-item v-for="(item,index) of list" :key="index" :content="item" :index="index" @deleteTodoList="delList">{{content}}</todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoItem from './components/todoItem'
+export default {
+ components: {
+    'todo-item': todoItem
+ },
+ data(){
+      return {
+        inputvalue: 'abc',
+        list: []
+      }
+      
+  },
+  
+  methods: {
+    handelTodoList(){
+      this.list.push(this.inputvalue);
+      this.inputvalue = '';
+    },
+    delList(index){
+      this.list.splice(index,1);
+    }
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <li class="todo-item" @click="delTodo">{{content}}</li>
+
+</template>
+
+<script>
+export default {
+  props: ['content','index'],
+  methods: {
+    delTodo(){
+      this.$emit('deleteTodoList',this.index)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .todo-item{
+    color: red;
+  }
+</style>
